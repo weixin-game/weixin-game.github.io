fba1b79a4ccde3bef52902040635374006a0fe36
weixin-game <85097980+weixin-game@users.noreply.github.com>
2021-05-31 15:00:03 +0800

    Add files via upload

---
+++ precache-manifest.eeca82a2c426a1f8f1e73c4d35f1e7f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaa04d0167eaf4cc7b2e327df117d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896dcb3397505f5bf8bc",
+    "url": "/static/css/main.c8f46f30.chunk.css"
+  },
+  {
+    "revision": "e319e3310f2efc6426e7",
+    "url": "/static/js/2.1c6eeed0.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/static/js/2.1c6eeed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896dcb3397505f5bf8bc",
+    "url": "/static/js/main.472b3de9.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
